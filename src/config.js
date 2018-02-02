30724a663d642443b36ef8bdc445011be0071bc8
Annie Chiang <devbear22@gmail.com>
2018-02-02 14:52:35 -0800

    update config to add netlfy as client origin option

--- src/config.js
+++ src/config.js
@@ -6,10 +6,9 @@ const TEST_DATABASE_URL = process.env.TEST_DATABASE_URL ||
                           'mongodb://localhost/test-guestbook';
 
 const PORT = process.env.PORT || 8080;
-const JWT_SECRET = 'ManageGuestbook2017';
+const JWT_SECRET = process.env.JWT_SECRET; //'ManageGuestbook2017';
 const JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
-// client origin needs to eventually be a client variable 
-const CLIENT_ORIGIN = "http://localhost:3000"; // or use create react app 
+const CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000';
 
 module.exports = { 
   CLIENT_ORIGIN, 
