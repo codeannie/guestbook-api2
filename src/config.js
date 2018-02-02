fd757dc45ade9eceeac1efb33a81f1974cd42565
Annie Chiang <devbear22@gmail.com>
2018-02-02 15:47:10 -0800

    change to local client

--- src/config.js
+++ src/config.js
@@ -8,7 +8,7 @@ const TEST_DATABASE_URL = process.env.TEST_DATABASE_URL ||
 const PORT = process.env.PORT || 8080;
 const JWT_SECRET = 'ManageGuestbook2017';
 const JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
-const CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000';
+const CLIENT_ORIGIN = 'http://localhost:3000' // process.env.CLIENT_ORIGIN || 
 
 module.exports = { 
   CLIENT_ORIGIN, 
